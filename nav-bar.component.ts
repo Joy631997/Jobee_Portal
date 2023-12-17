63fd273e6895715c1755711c3618d128fbab86e6
Joy631997 <115701680+Joy631997@users.noreply.github.com>
2023-12-17 00:10:34 -0800

    navbar
    navbar files added

---
+++ nav-bar.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import {MatIconModule} from '@angular/material/icon';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatButtonModule} from '@angular/material/button';
+
+@Component({
+  selector: 'app-nav-bar',
+  templateUrl: './nav-bar.component.html',
+  styleUrls: ['./nav-bar.component.css'],
+  // imports: [MatButtonModule, MatDividerModule, MatIconModule],
+  // standalone: true
+  
+})
+export class NavBarComponent {
+
+}
+ 
