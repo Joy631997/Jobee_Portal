d93653d7c93b9dcbc528d853a125e7d90dc34f96
Joy631997 <115701680+Joy631997@users.noreply.github.com>
2023-12-17 00:12:17 -0800

    wellbeing
    wellbeing files added

---
+++ well-being.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-well-being',
+  templateUrl: './well-being.component.html',
+  styleUrls: ['./well-being.component.css']
+})
+export class WellBeingComponent {
+
+}
